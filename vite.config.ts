aabe88a757ac55667a23edc211e9ddc8670b7e1f
AmaroPedroJr <47424452+AmaroPSJunior@users.noreply.github.com>
2026-05-01 07:11:10 -0300

    Sync via webOS Workspace - 5/1/2026, 10:11:09 AM

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,17 @@
-import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
-import {defineConfig, loadEnv} from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import legacy from '@vitejs/plugin-legacy';
 
-export default defineConfig(({mode}) => {
+export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [
-      react(), 
-      tailwindcss(),
       legacy({
         targets: ['chrome > 38', 'safari > 7'],
         additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
-        renderModernChunks: true
+        renderModernChunks: false // Reduz complexidade de script
       })
     ],
     base: "./",
-    define: {
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-    },
     build: {
       target: 'chrome38',
       outDir: 'dist',
@@ -38,15 +30,10 @@ export default defineConfig(({mode}) => {
         safari10: true
       }
     },
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, '.'),
-      },
-    },
     server: {
-      // HMR is disabled in AI Studio via DISABLE_HMR env var.
-      // Do not modifyâfile watching is disabled to prevent flickering during agent edits.
+      port: 3000,
+      host: '0.0.0.0',
       hmr: process.env.DISABLE_HMR !== 'true',
-    },
+    }
   };
 });
